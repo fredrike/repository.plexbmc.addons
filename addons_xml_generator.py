0a0709c109348e9fa22d42cb29de58f698374c36
Fredrik Erlandsson <fredrik.e@gmail.com>
2011-03-21 10:19:08 +0100

    Merge of archive.py and addons_xml_generator.py.

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -2,6 +2,9 @@
 
 import os
 import md5
+import xml.dom.minidom
+import zipfile
+import glob
 
 
 class Generator:
@@ -67,7 +70,24 @@ class Generator:
             # oops
             print "An error occurred saving %s file!\n%s" % ( file, e, )
 
+def recursive_zip(zipf, directory, folder=""):
+	for item in os.listdir(directory):
+		if not item.endswith(".zip"):
+			if os.path.isfile(os.path.join(directory, item)):
+				zipf.write(os.path.join(directory, item), folder + os.sep + item, zipfile.ZIP_DEFLATED)
+			elif os.path.isdir(os.path.join(directory, item)):
+				recursive_zip(zipf, os.path.join(directory, item), folder + os.sep + item)
+
 
 if ( __name__ == "__main__" ):
     # start
-    Generator()
+		Generator()
+ 
+		for addon in xml.dom.minidom.parse("addons.xml").getElementsByTagName("addon"):
+			name = addon.getAttribute("id") + "-" + addon.getAttribute("version") + ".zip"
+			print "Creating archive: " + name
+			zipf = zipfile.ZipFile(os.path.join(addon.getAttribute("id"), name), "w")
+			path = addon.getAttribute("id")
+			recursive_zip(zipf, path, addon.getAttribute("id"))
+	 		zipf.close()
+
